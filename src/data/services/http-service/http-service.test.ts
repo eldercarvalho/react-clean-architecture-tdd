b168e7ec14a28bd50eca79257438f2527132e46c
Elder Carvalho <eld.carv@gmail.com>
2022-06-02 02:20:27 -0300

    refactor: move HttpService to its own file

--- src/data/services/http-service/http-service.test.ts
+++ src/data/services/http-service/http-service.test.ts
@@ -1,30 +1,11 @@
+import { anything, instance, mock, reset, verify, when } from 'ts-mockito';
 import { IHttpClient } from '@/data/protocols';
 import { IMovie, IPaginatedResult } from '@/domain/entities';
 import { AppError, NotFoundError, UnauthorizedError } from '@/domain/errors';
 import { paginatedResultMock } from '@/domain/mocks';
-import { anything, instance, mock, reset, verify, when } from 'ts-mockito';
-import { IHttpService } from './http-service';
+import { HttpService } from './http-service';
 
 const HttpClientMock = mock<IHttpClient>();
-
-class HttpService implements IHttpService {
-  constructor(private readonly httpClient: IHttpClient) {}
-
-  async getMovies(): Promise<IPaginatedResult<IMovie[]>> {
-    const response = await this.httpClient.get<IPaginatedResult<IMovie[]>>('/movie/popular');
-
-    switch (response.statusCode) {
-      case 200:
-        return response.data;
-      case 401:
-        throw new UnauthorizedError();
-      case 404:
-        throw new NotFoundError();
-      default:
-        throw new AppError();
-    }
-  }
-}
 
 describe('HttpService', () => {
   beforeEach(() => {
--- src/data/services/http-service/http-service.ts
+++ src/data/services/http-service/http-service.ts
@@ -1,5 +1,26 @@
+import { IHttpClient } from '@/data/protocols';
 import { IMovie, IPaginatedResult } from '@/domain/entities';
+import { AppError, NotFoundError, UnauthorizedError } from '@/domain/errors';
 
 export interface IHttpService {
   getMovies(): Promise<IPaginatedResult<IMovie[]>>;
+}
+
+export class HttpService implements IHttpService {
+  constructor(private readonly httpClient: IHttpClient) {}
+
+  async getMovies(): Promise<IPaginatedResult<IMovie[]>> {
+    const response = await this.httpClient.get<IPaginatedResult<IMovie[]>>('/movie/popular');
+
+    switch (response.statusCode) {
+      case 200:
+        return response.data;
+      case 401:
+        throw new UnauthorizedError();
+      case 404:
+        throw new NotFoundError();
+      default:
+        throw new AppError();
+    }
+  }
 }
