cbb9afe810db9709a5b895b3acda31d98f5868d8
Elder Carvalho <eld.carv@gmail.com>
2022-06-03 01:16:07 -0300

    feat: define HttpClient baseUrl and params

--- src/infra/http-client/http-client.ts
+++ src/infra/http-client/http-client.ts
@@ -2,7 +2,12 @@ import { IHttpClient, IHttpClientResponse } from '@/data/protocols';
 import { AxiosInstance } from 'axios';
 
 export class HttpClient implements IHttpClient {
-  constructor(private readonly axiosInstance: AxiosInstance) {}
+  constructor(private readonly axiosInstance: AxiosInstance) {
+    this.axiosInstance.defaults.baseURL = 'https://api.themoviedb.org/3';
+    this.axiosInstance.defaults.params = {
+      api_key: 'a188c505472d557d08c1cc8bc7c0ea10',
+    };
+  }
 
   async get<T>(path: string): Promise<IHttpClientResponse<T>> {
     const response = await this.axiosInstance.get(path);
